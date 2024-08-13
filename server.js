90c1226771ce9c9831ad3c489c37f4383209cf14
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-13 11:14:24 -0300

    :recycle: Refatorando servidor: Usando o Express

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Express API",
-    "/livros": "Entrei na rota Livros",
-    "/autores": "Entrei na rota Autores"
-}
+// const rotas = {
+//     "/": "Curso de Express API",
+//     "/livros": "Entrei na rota Livros",
+//     "/autores": "Entrei na rota Autores"
+// }
 
-const server = http.createServer( (req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end(rotas[req.url]);
-});
+// const server = http.createServer( (req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, ()=> {
+app.listen(PORT, ()=> {
     console.log("Servidor escutando!");
 }); 

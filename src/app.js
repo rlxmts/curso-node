d326db27f1a5f351ad9deec27cb490381e78659b
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-13 11:40:18 -0300

    :sparkles: Criando a rota Livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "É assim que acaba"
+    },
+    {
+        id: 2,
+        titulo: "Cangaceiro JavaScript"
+    }
+]
+
 app.get("/", (req, res)=> {
     res.status(200).send("Curso de node da Alura");
+});
+
+app.get("/livros", (req, res)=> {
+    res.status(200).json(livros);
 });
 
 export default app;

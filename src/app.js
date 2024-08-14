36070f055ee2668458a9df7fb234a95f01ae311d
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-14 15:57:44 -0300

    :broom: Eliminando codigos comentados do inicio do projeto

--- src/app.js
+++ src/app.js
@@ -6,63 +6,10 @@ const conexao = await conectaDatabase();
 
 conexao.on("error", (erro)=> {
     console.error("Erro de conexão: ", erro);
-})
-
+});
 conexao.once("open", ()=> {
     console.log("conexão bem sucedida!")
-})
-
+});
 const app = express();
 routes(app);
-
-// app.use(express.json());
-
-// const livros = [
-//     {
-//         id: 1,
-//         titulo: "É assim que acaba"
-//     },
-//     {
-//         id: 2,
-//         titulo: "Cangaceiro JavaScript"
-//     }
-// ]
-
-// const buscaLivro = (id)=> {
-//     return livros.findIndex( livro => {
-//         return livro.id === Number(id);
-//     })
-// }
-
-// app.get("/", (req, res)=> {
-//     res.status(200).send("Curso de node da Alura");
-// });
-
-// // app.get("/livros", async (req, res)=> {
-// //     const listaLivros = await livro.find({});
-// //     res.status(200).json(listaLivros);
-// // });
-
-// app.post("/livros", (req, res) => {
-//     livros.push(req.body);
-//     res.status(201).send("Livro cadastrado com sucesso!");
-// });
-
-// app.get("/livros/:id", (req, res) => {
-//     const index = buscaLivro(req.params.id);
-//     res.status(200).json(livros[index]);
-// });
-
-// app.put("/livros/:id", (req, res)=> {
-//     const index = buscaLivro(req.params.id);
-//     livros[index].titulo = req.body.titulo;
-//     res.status(200).json(livros);
-// });
-
-// app.delete("/livros/:id", (req, res)=> {
-//     const index = buscaLivro(req.params.id);
-//     livros.splice(index, 1); 
-//     res.status(200).send("Livro deletado com sucesso!");
-// });
-
 export default app;

c68bec896d2abf5224b70a9efe65df42abd93361
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-14 12:33:18 -0300

    :sparkles: Criando controller CadastrarLivros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -5,6 +5,15 @@ class LivroController {
     static async listarLivros (req, res) {
         const listaLivros = await livro.find({});
         res.status(200).json(listaLivros);
+    };
+
+    static async cadastrarLivro (req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({ message: "Criado com sucesso!", Livro: novoLivro });
+        } catch(erro){
+            res.status(500).send("Erro ao conectar: ", erro.message);
+        }
     }
 }
 

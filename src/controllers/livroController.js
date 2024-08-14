97c7cf68d3fb216800ea8ca79e4b5b1323b22c1c
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-14 15:53:11 -0300

    :sparkles: Adicionando metodos/ cadastrar / editar / deletar

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,10 +3,24 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try{
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch(erro){
+            res.status(500).send("Erro ao conectar: ", erro.message);
+        }
     };
-
+    
+    static async buscaLivroId (req, res) {
+        try{
+            const id = req.params.id;
+            const livroFiltrado = await livro.findById(id);
+            res.status(200).json(livroFiltrado);
+        } catch(erro){
+            res.status(500).send("Erro ao buscar livro: ", erro.message);
+        }
+    };    
+    
     static async cadastrarLivro (req, res) {
         try {
             const novoLivro = await livro.create(req.body);
@@ -14,7 +28,27 @@ class LivroController {
         } catch(erro){
             res.status(500).send("Erro ao conectar: ", erro.message);
         }
-    }
+    };
+    
+    static async editarLivro (req, res) {
+        try{
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado com sucesso!" });
+        } catch(erro){
+            res.status(500).send("Erro ao atualizar livro: ", erro.message);
+        }
+    }; 
+    
+    static async deletarLivro (req, res) {
+        try{
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id, req.body);
+            res.status(200).json({ message: "Livro deletado com sucesso!" });
+        } catch(erro){
+            res.status(500).send("Erro ao deletar livro: ", erro.message);
+        }
+    }; 
 }
 
 export default LivroController;

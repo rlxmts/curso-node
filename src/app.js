70079a3bab4abea681108dd1e38556ed99d18f05
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-14 12:38:52 -0300

    :recycle: Refatorando app.js/ Iniciando Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaDatabase();
 
@@ -13,54 +13,56 @@ conexao.once("open", ()=> {
 })
 
 const app = express();
-app.use(express.json());
+routes(app);
 
-const livros = [
-    {
-        id: 1,
-        titulo: "É assim que acaba"
-    },
-    {
-        id: 2,
-        titulo: "Cangaceiro JavaScript"
-    }
-]
+// app.use(express.json());
 
-const buscaLivro = (id)=> {
-    return livros.findIndex( livro => {
-        return livro.id === Number(id);
-    })
-}
+// const livros = [
+//     {
+//         id: 1,
+//         titulo: "É assim que acaba"
+//     },
+//     {
+//         id: 2,
+//         titulo: "Cangaceiro JavaScript"
+//     }
+// ]
 
-app.get("/", (req, res)=> {
-    res.status(200).send("Curso de node da Alura");
-});
+// const buscaLivro = (id)=> {
+//     return livros.findIndex( livro => {
+//         return livro.id === Number(id);
+//     })
+// }
 
-app.get("/livros", async (req, res)=> {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-});
+// app.get("/", (req, res)=> {
+//     res.status(200).send("Curso de node da Alura");
+// });
 
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!");
-});
+// // app.get("/livros", async (req, res)=> {
+// //     const listaLivros = await livro.find({});
+// //     res.status(200).json(listaLivros);
+// // });
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
+// app.post("/livros", (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send("Livro cadastrado com sucesso!");
+// });
 
-app.put("/livros/:id", (req, res)=> {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-});
+// app.get("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);
+//     res.status(200).json(livros[index]);
+// });
 
-app.delete("/livros/:id", (req, res)=> {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1); 
-    res.status(200).send("Livro deletado com sucesso!");
-});
+// app.put("/livros/:id", (req, res)=> {
+//     const index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo;
+//     res.status(200).json(livros);
+// });
+
+// app.delete("/livros/:id", (req, res)=> {
+//     const index = buscaLivro(req.params.id);
+//     livros.splice(index, 1); 
+//     res.status(200).send("Livro deletado com sucesso!");
+// });
 
 export default app;

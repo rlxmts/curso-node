1830d99ddec2f3059d5c01113c1cb4aaaea81c04
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-14 15:52:19 -0300

    :sparkles: Finalizando crud com routes

--- src/routes/LivrosRoutes.js
+++ src/routes/LivrosRoutes.js
@@ -2,4 +2,8 @@ import express from "express";
 import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.buscaLivroId);
+routes.put("/livros/:id", LivroController.editarLivro);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.delete("/livros/:id", LivroController.deletarLivro);
 export default routes;
